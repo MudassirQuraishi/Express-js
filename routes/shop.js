91429cd3425f1f80e2fd2f5766f549eb1c8bca54
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-08-03 11:04:10 +0530

    Serving html files using path module

--- routes/shop.js
+++ routes/shop.js
@@ -1,24 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 
 
-const landingPageCode = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>App</title>
-</head>
-<body>
-    <h1>Welcome to Landing Page</h1>
-    <form action="/shop" method="post"><a href="/admin/add-product">Add Product</a></form>
-</body>
-</html>`;
-
-router.get('/',(req,res,next)=>{
-    res.send(landingPageCode);
+router.use('/',(req,res,next)=>{
+    res.sendFile(path.join(__dirname, '../','views','shop.html'));
 });
 
 module.exports=router;
